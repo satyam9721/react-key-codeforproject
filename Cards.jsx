164b1b29c9feacc5429ef2f0ed2db48972db5e84
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-02-23 13:03:56 +0530

    Update Cards.jsx

--- Cards.jsx
+++ Cards.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 export default function Cards(props) {
   //with the help of props, we are fetching data from database.
@@ -6,44 +6,17 @@ export default function Cards(props) {
   let options = props.options;
   let priceOptions = Object.keys(options); //displaying data half and full from database
 
-  return (
-    <div>
-      <div>
-        <div className="card" style={{ width: "18rem", maxHeight: "360px" }}>
+ 
           <img
             src={props.imgSrc} //imoprting diffrent image from to database
             className="card-img-top"
-            alt="..." style={{height:"120px",objectFit:"fill"}} //this line fit each photo in organized way
-          />
-          <div className="card-body">
-            <h5 className="card-title">{props.foodName}</h5>
-            <p className="card-text">This is some Important text.</p>
-            <div className="container w-100">
-              <select className="m-2 h-100  bg-secondary">
-                {Array.from(Array(6), (e, i) => {
-                  return (
-                    <option key={i + 1} value={i + 1}>
-                      {" "}
-                      {i + 1}
-                    </option>
-                  );
-                })}
-              </select>
-              <select className="m-2 h-100  bg-secondary rounded">
+            alt="..." style={{height:"120px",objectFit:"fill"}} //this line fit each photo in organized way/>
+          
                 {/* selecting quantity of food */}
                 {priceOptions.map((data) => {
                   return (
                     <options key={data} value={data}>
                       {data}
                     </options>
-                  );
-                })}
-              </select>
-              <div className="d-inline h-100 fs-5">Total Price</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+                 
+<ul className="navbar-nav me-auto mb-2"> //shifting login and signup button on right side
